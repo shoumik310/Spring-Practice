f7defc702221bb9852b27146f8bf829a4e53d3b1
pradeep <pradeep.kumar44@gmail.com>
2020-04-10 14:51:53 +0530

    initial commit

---
+++ src/main/java/com/classpath/assetmgmt/AssetMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.classpath.assetmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetMgmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetMgmtApplication.class, args);
+    }
+
+}
