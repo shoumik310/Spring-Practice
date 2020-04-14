a6017129b56d0b8c963bf8acc10a6f8cbe35fd3e
pradeep <pradeep.kumar44@gmail.com>
2020-04-14 14:52:36 +0530

    committing the changes

--- src/main/java/com/classpath/ordermgmt/model/Order.java
+++ src/main/java/com/classpath/ordermgmt/model/Order.java
@@ -1,18 +1,25 @@
 package com.classpath.ordermgmt.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Objects;
+import java.time.LocalDate;
 import java.util.Set;
-
 import static javax.persistence.CascadeType.*;
 import static javax.persistence.FetchType.*;
 
 @Entity
 @Table(name = "orders")
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode(of = "orderId")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Order{
 
@@ -22,64 +29,25 @@ public class Order{
     @JsonProperty("order_id")
     private long orderId;
 
-    private double price;
+    @Column(name = "order_date")
+    @JsonProperty("order_date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yy")
+    private LocalDate orderDate;
+
+    @Column(name="merchant_name")
+    @JsonProperty("merchant_name")
+    private String merchantName;
 
     @OneToMany(mappedBy = "order",
-            fetch = EAGER,
-            cascade = CascadeType.ALL )
-    @JsonProperty("order_line_items")
+            fetch = LAZY,
+            cascade = {PERSIST, REMOVE})
+    @JsonProperty("line_items")
     @Column(name = "line_items")
     private Set<OrderLineItem> orderLineItems;
-
-    public long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(long orderId) {
-        this.orderId = orderId;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public Set<OrderLineItem> getOrderLineItems() {
-        return orderLineItems;
-    }
-
-    public void setOrderLineItems(Set<OrderLineItem> orderLineItems) {
-        this.orderLineItems = orderLineItems;
-    }
 
     // scaffolding code
     public void addOrderLineItem(OrderLineItem orderLineItem){
         this.orderLineItems.add(orderLineItem);
         orderLineItem.setOrder(this);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Order order = (Order) o;
-        return orderId == order.orderId;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(orderId);
-    }
-
-    @Override
-    public String toString() {
-        return "Order{" +
-                "orderId=" + orderId +
-                ", price=" + price +
-                ", orderLineItems=" + orderLineItems +
-                '}';
     }
 }
--- src/main/java/com/classpath/ordermgmt/model/OrderLineItem.java
+++ src/main/java/com/classpath/ordermgmt/model/OrderLineItem.java
@@ -3,85 +3,43 @@ package com.classpath.ordermgmt.model;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.*;
 
 import java.util.Objects;
 
+import static javax.persistence.GenerationType.*;
+
 @Entity
 @Table(name = "order_line_item")
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Setter
+@Getter
+@ToString(exclude = "order")
+@EqualsAndHashCode(exclude = "price")
 public class OrderLineItem {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = AUTO)
     private long id;
     private String name;
     private double price;
+    private int quantity;
 
     @ManyToOne
     @JoinColumn(name="order_id", nullable = false)
     @JsonIgnore
     private Order order;
 
+    //default constructor
+    OrderLineItem(){}
+
     public OrderLineItem(String name, double price){
         this.name = name;
         this.price = price;
-    }
-
-    public OrderLineItem(){}
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public Order getOrder() {
-        return order;
-    }
-
-    public void setOrder(Order order) {
-        this.order = order;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        OrderLineItem that = (OrderLineItem) o;
-        return id == that.id &&
-                Objects.equals(name, that.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-    @Override
-    public String toString() {
-        return "OrderLineItem{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price=" + price +
-                '}';
     }
 }
