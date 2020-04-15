a21db269f1146d1d8f02aaa0847215f629f1a21f
pradeep <pradeep.kumar44@gmail.com>
2020-04-15 12:56:53 +0530

    committing with authorization rules

--- src/main/java/com/classpath/ordermgmt/config/SecurityConfiguration.java
+++ src/main/java/com/classpath/ordermgmt/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.classpath.ordermgmt.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,13 +11,34 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static org.springframework.http.HttpMethod.*;
+
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
     // setup users and permissions
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        // configure authorization rules here
+        httpSecurity.cors().disable();
+        httpSecurity.csrf().disable();
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers(GET,"/v1/orders/**")
+                .hasAnyRole("USER")
+                .and()
+                .authorizeRequests()
+                .antMatchers(POST,"/v1/orders/**")
+                .hasRole("ADMIN")
+                .antMatchers(PUT,"/v1/orders/**")
+                .hasRole("ADMIN")
+                .antMatchers(DELETE,"/v1/orders/**")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
     }
 
     @Override
