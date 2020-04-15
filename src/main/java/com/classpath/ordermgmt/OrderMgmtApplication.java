caf29ebddd02a9758d3a62a19e823a35d545a14c
pradeep <pradeep.kumar44@gmail.com>
2020-04-15 11:37:44 +0530

    committing the spring security scaffolding code

--- src/main/java/com/classpath/ordermgmt/OrderMgmtApplication.java
+++ src/main/java/com/classpath/ordermgmt/OrderMgmtApplication.java
@@ -3,9 +3,11 @@ package com.classpath.ordermgmt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableWebSecurity
 public class OrderMgmtApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderMgmtApplication.class, args);
