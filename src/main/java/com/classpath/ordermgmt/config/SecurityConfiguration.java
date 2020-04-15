b95c655c19305378ce644609f59db1d121dee7f6
pradeep <pradeep.kumar44@gmail.com>
2020-04-15 11:59:55 +0530

    committing the inmemory authentication

--- src/main/java/com/classpath/ordermgmt/config/SecurityConfiguration.java
+++ src/main/java/com/classpath/ordermgmt/config/SecurityConfiguration.java
@@ -1,14 +1,38 @@
 package com.classpath.ordermgmt.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    // setup users and permissions
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        //configure users
+        authenticationManagerBuilder
+                .inMemoryAuthentication()
+                .withUser("kiran")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .and()
+                .withUser("vinay")
+                .password(passwordEncoder().encode("adminuser"))
+                .roles("USER","ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
