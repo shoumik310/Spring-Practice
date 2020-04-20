879dc4b11c7a291024cd821a4e69704cf9341919
pradeep <pradeep.kumar44@gmail.com>
2020-04-20 10:01:19 +0530

    profile added so that the cors configuration will be only active in dev profile

--- src/main/java/com/classpath/ordermgmt/config/CorsConfiguration.java
+++ src/main/java/com/classpath/ordermgmt/config/CorsConfiguration.java
@@ -1,10 +1,12 @@
 package com.classpath.ordermgmt.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("dev")
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
