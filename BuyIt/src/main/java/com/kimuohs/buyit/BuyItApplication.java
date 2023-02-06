8a74024d4a3c9e1f0b3b241d92374e532523ab94
Shoumik Panandikar <shoumikrp2000@gmail.com>
2023-02-06 20:48:03 +0530

    ECommerce Starter

---
+++ BuyIt/src/main/java/com/kimuohs/buyit/BuyItApplication.java
@@ -0,0 +1,13 @@
+package com.kimuohs.buyit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyItApplication.class, args);
+	}
+
+}
---
+++ BuyIt/src/test/java/com/kimuohs/buyit/BuyItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimuohs.buyit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
